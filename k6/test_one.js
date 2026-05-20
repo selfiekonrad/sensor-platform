5f1de0422df107f9ea29e17a051c43f3c05bf24d
Konrad <konradceniuch@icloud.com>
2026-05-20 15:39:48 +0200

    add k6 test scripts for API registration and Grafana status check

--- k6/test_one.js
+++ k6/test_one.js
@@ -1,10 +1,23 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import {sleep, check} from 'k6';
 
 export const options = {
-    iterations: 10,
+    iterations: 1,
 };
 
-export default function() {
-    http.post
+export default function () {
+    let res =
+        http.post('http://localhost:3435/api/register')
+            .body(
+                JSON.stringify({
+                    sensorRegistryData: {
+                        name: 'k6-sensor',
+                        type: 'e58ed763-928c-4155-bee9-fdbaaadc15f3',
+                    }
+                })
+            )
+            .header('Content-Type', 'application/json');
+
+    check(res, { "status is 200": (res) => res.status === 200 });
+    sleep(1);
 }

79b932a755fc02842736109bd447955cb3956d34
Konrad <konradceniuch@icloud.com>
2026-05-24 18:46:32 +0200

    version update

--- k6/last_test.ts
+++ k6/last_test.ts
@@ -5,7 +5,7 @@ import http from 'k6/http';
 export let options:Options = {
     vus: 50,
     duration: '10s',
-    iterations: 1,
+    iterations: 50,
     hosts: {
         'sensor-platform.local': '127.0.0.1',
     },
